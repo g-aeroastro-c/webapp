2bbafed483536f6b1c1a80f8ee7a06e8dd7df0f5
kamalsushank <kamalsushank@gmail.com>
2025-08-13 21:56:34 +0530

    Changes in the astronomy page - rectified some problems related to z index

--- src/app/teams/astronomy/page.tsx
+++ src/app/teams/astronomy/page.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 
 const AstronomyTeamPage = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 text-white ">
+    <div className="min-h-screen bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 text-white">
       <div className="container mx-auto py-20 px-4 text-center relative z-30">
-        <h1 className="text-5xl font-bold mt-8 mb-8 relative z-30">
+        <h1 className="text-5xl font-bold mt-8 mb-8 relative z-40">
           Astronomy Team
         </h1>
-        <p className="mb-8 text-xl relativez-20">
+        <p className="mb-8 text-xl relative z-20">
           Welcome to the Astronomy Team page. Our team is dedicated to exploring
           the wonders of the universe and sharing our knowledge with others.
           Join us in our celestial journey!
         </p>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 relative z-20">
-          <div className="bg-white text-black p-6 rounded-lg shadow-md transform transition duration-500 hover:scale-105 relative z-20 ">
+          <div className="bg-white text-black p-6 rounded-lg shadow-md transform transition duration-500 hover:scale-105 relative z-20">
             <h2 className="text-2xl font-bold mb-4">Stargazing Nights</h2>
             <p className="mb-4">
               Join us for our monthly stargazing sessions where we explore the
